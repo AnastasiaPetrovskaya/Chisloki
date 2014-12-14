385949a053b24b4b7d652e49fcb7273c6af1346f
AnastasiaPetrovskaya <sweet_chery2010@mail.ru>
2014-12-14 17:42:15 +0300

    .

--- PetrovskayaMatrix/Methods.cs
+++ PetrovskayaMatrix/Methods.cs
@@ -29,7 +29,7 @@ namespace PetrovskayaMatrix
                 for(int j = 0; j < X2.VectorGetSet.GetLength(0); j++)
 			    {
 			        delta += (X2.VectorGetSet[j] - X1.VectorGetSet[j])*(X2.VectorGetSet[j] - X1.VectorGetSet[j]);
-                    j ++;
+                    
 			    } 
                 if (Math.Sqrt(delta) < epsilon) // если заданная точность достигнута
                 {
@@ -54,32 +54,30 @@ namespace PetrovskayaMatrix
             Matrix D = A.GenDModified();
             Vector X1 = new Vector(b.VectorGetSet);
             Vector X2 = new Vector(X1.VectorGetSet);
-            //double tau =  0.5 / (A.Norm + 1);
-            double tau = Math.Abs((1 - D.Norm) / A.Norm);
+            double tau =  2 / (A.Norm + 1);
+            //double tau = Math.Abs((1 - D.Norm) / A.Norm);
             Matrix inv = D.Inverse();
-            bool flag ;
             while (countIterations <= 1000000) // i - максимально допустимое количество итераций
             {
-                flag = true;
                 //X2 = inv.MultOnVect(A.MultOnVect(X1).MultOnNum(-1).SumVector(b)).MultOnNum(tau).SumVector(X1);
                 X2 = inv.MultOnVect(D.SumMatrixes(A.MultOnNum(-tau)).MultOnVect(X1).SumVector(b.MultOnNum(tau)));
                 // проверка разности межну решением на предыдущей итерации и на текущей  
-                for (int j = 0; j < X1.VectorGetSet.Length; j++)
+                // проверка разности межну решением на предыдущей итерации и на текущей  
+                double delta = 0;
+                for (int j = 0; j < X2.VectorGetSet.GetLength(0); j++)
                 {
-                    if (Math.Abs(X1.SumVector(X2.MultOnNum(-1)).VectorGetSet[j]) > epsilon) flag = false;
+                    delta += (X2.VectorGetSet[j] - X1.VectorGetSet[j]) * (X2.VectorGetSet[j] - X1.VectorGetSet[j]);
+
                 }
-                
-                if (flag) // если заданная точность достигнута
+                if (Math.Sqrt(delta) < epsilon) // если заданная точность достигнута
                 {
                     return X2;
                 }
                 else
                 {
-                    Array.Copy(X2.VectorGetSet, X1.VectorGetSet, X1.VectorGetSet.Length); ;
-                    //if (countIterations % 500 == 0)
-                        //System.Windows.Forms.MessageBox.Show(X1.toString());
+                    X1 = X2;
                     countIterations++;
-                }
+                }   
             }
             throw new Exception("модифицированный метод простой итерации не сошелся");
         }
